e06818478b6f691af5e0cfdb7f8f2ccddfdce5f9
ryampeguero <ryampeguero@gmail.com>
2024-04-29 22:34:56 +0200

    feat: milestone 1 and bonus done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-           mail: "",
+           mailCont: [],
+           counterControl: 0,
         }
     },
 
@@ -12,19 +13,13 @@ createApp({
         for (let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
                 console.log(resp.data.response);
-                this.mail = resp.data.response;
-                });
+                this.mailCont.push(resp.data.response);
+                this.counterControl++;
+                console.log(this.counterControl);
+            });
             
         }
         
     },
-
-    computed: {
-
-    },
-
-    methods: {
-
-    }
     
 }).mount("#app");
